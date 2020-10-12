027c7c25af9861031ec574ebfc1fedb03b4e96b1
Anant Shankhdhar <anantshankhdhar0808@gmail.com>
2020-10-13 02:53:52 +0530

    deleted some stuff

--- mystery_rooms/urls.py
+++ mystery_rooms/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.room, name='room'),
-    path('door/',views.door,name='door'),
-    path('paintings/',views.paintings,name='painting'),
-    path('locker/',views.locker,name='locker'),
-    path('cupboard/',views.cupboard,name='cupboard'),
+    
 ]
